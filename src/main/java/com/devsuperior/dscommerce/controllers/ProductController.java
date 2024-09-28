a258a1b9f16bc161864958c52b84b03a533fb206
GABRIEL LUCAS <119793695+gabriellglrs@users.noreply.github.com>
2024-09-27 23:10:42 -0300

    modified Metodo insert POST Pruduct

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -5,7 +5,11 @@ import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -15,18 +19,28 @@ public class ProductController {
      private ProductService service;
 
      @GetMapping(value = "/{id}")
-     public ProductDTO findById(@PathVariable Long id) {
-          return service.findById(id);
+     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+         ProductDTO dto = service.findById(id);
+         return ResponseEntity.ok(dto);
      }
 
      @GetMapping
-     public Page<ProductDTO> findAll(Pageable pageable) {
-          return service.findAll(pageable);
+     public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+          Page<ProductDTO> dto = service.findAll(pageable);
+          return ResponseEntity.ok(dto);
      }
 
      @PostMapping
-     public ProductDTO insert(@RequestBody ProductDTO productDTO) {
-          return service.insert(productDTO);
+     public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+          dto = service.insert(dto);
+          URI uri = ServletUriComponentsBuilder
+                  .fromCurrentRequest()
+                  .path("/{id}")
+                  .buildAndExpand(dto.getId())
+                  .toUri();
+          return ResponseEntity
+                  .created(uri)
+                  .body(dto);
      }
 
 }
